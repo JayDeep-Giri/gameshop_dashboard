27acf35a47d39db92cb99ab079e7f76a10b364e9
JayDeep-Giri <jaydeepgiri30@gmail.com>
2023-11-12 14:42:12 +0530

    Initialized GameShop Dashboard project

---
+++ src/components/GameSlide.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+//import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/navigation';
+
+import './gameSwiper.css';
+
+const GameSlide = ({ game, active, toggleVideo }) => {
+  return (
+    <div className="gameSlider">
+        <img src={game.img} alt='Game Img'/>
+        <div className={`video ${active ? 'active' : undefined}`}>
+            <iframe width='1280'
+            height='720'
+            src={game.trailer}
+            title={game.title}
+            allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;'
+            allowFullScreen />
+        </div>
+        <div className="content">
+            <h2>{game.title}</h2>
+            <p>{game.description}</p>
+            <div className="buttons">
+                <a href='#' className='orderBtn'>Order Now</a>
+                <a href='#' className={`playBtn ${active ? 'active' : undefined}`} onClick={toggleVideo}>
+                    <span className='pause'><i className='bi bi bi-pause-fill' /></span>
+                    <span className='play'><i className='bi bi bi-play-fill' /></span>
+                </a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default GameSlide;
---
+++ src/pages/Header.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
+import './header.css';
+
+const Header = ({ toggleActive }) => {
+    const { library, bag } = useContext(AppContext);
+
+
+  return (
+    <header>
+        <a href="#" className="menu" onClick={toggleActive}>
+            <i className='bi bi-sliders' />
+        </a>
+        <div className="userItems">
+            <a href="#" className="icon">
+                <i className='bi bi-heart-fill' />
+                <span className='like'>{library.length}</span>
+            </a>
+            <a href="#" className="icon">
+                <i className='bi bi-bag-fill' />
+                <span className='bag'>{bag.length}</span>
+            </a>
+            <div className='avatar'>
+                <a href="#">
+                    <img src='https://picsum.photos/seed/picsum/200/300' alt='user Image' />
+                </a>
+                <div className='user'>
+                    <span>User Name</span>
+                    <a href="#">View Profile</a>
+                </div>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
